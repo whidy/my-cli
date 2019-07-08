f340262aa7b3e8d6aa8509a29a828e842832e6cd
Whidy_Macbook <whidy@whidy.net>
2019-07-08 15:33:37 +0800

    增加postcss样式

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -9,6 +9,9 @@
         <p>{{ this.$store.state.counter }}</p>
         <el-button @click="$store.dispatch('SET_RANDOM')">add after 1s</el-button>
       </div>
+      <div class="test">
+        <p>blue font</p>
+      </div>
     </div>
   </div>
 </template>
@@ -29,7 +32,43 @@ export default {
   }
 }
 </script>
+<style lang="postcss" scoped>
+.test {
+  p {
+    color: blue;
+  }
+}
+</style>
 
 <style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
 
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
 </style>
